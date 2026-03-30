f7d2b9fc6ebe40fc531e43b1ec5d48ede73e9bea
doubletake012-art <doubletake012@gmail.com>
2026-03-31 03:42:18 +0800

    Add files via upload

---
+++ BIRTHDAY NI JELLA/script.js
@@ -0,0 +1,19 @@
+function showMessage() {
+    // Ipakita ang message
+    document.getElementById("message").style.display = "block";
+    
+    // Itago ang button
+    document.getElementById("surprise-btn").style.display = "none";
+
+    // Itago ang subtitle
+    document.getElementById("subtitle").style.display = "none";
+
+    // Itago ang image
+    document.getElementById("birthday-img").style.display = "none";
+}
+function showMessage() {
+    document.getElementById("message").style.display = "block";
+    document.getElementById("surprise-btn").style.display = "none";
+    document.getElementById("subtitle").style.display = "none";
+    document.getElementById("birthday-img").style.display = "none";
+}
